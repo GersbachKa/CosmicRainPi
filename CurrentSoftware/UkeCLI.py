6010239d62e44912858ea1647524681530fad922
Kyle Gersbach <Gersbachka@gmail.com>
2018-11-05 13:16:48 -0800

    Added a motor test to UkeCLI

--- CurrentSoftware/UkeCLI.py
+++ CurrentSoftware/UkeCLI.py
@@ -1,5 +1,6 @@
 #UkeCLI.py
 import os, Ukulele
+import ServoController as sc
 
 def main():
     print("Cosmic Rain Ukelele V0.1")
@@ -8,12 +9,12 @@ def main():
     
 def functionSelector(u):
     while(True):
-        print("\nAvailable Functions: \n1. Import Song, 2. List Songs, 3.Play Song")
+        print("\nAvailable Functions: \n1. Import Song, 2. List Songs, 3.Play Song, 4.Motor Test")
         try:
             i = int(input("Which Function, 0 to exit: "))
         except ValueError:
             i=-1
-        if(i<0 or i>3):
+        if(i<0 or i>4):
             #clear the screen somehow?
             print("Invalid function")
         elif(i==0):
@@ -68,6 +69,23 @@ def doFunction(u,i):
                 #print(s)
                 #print(e)
                 print(S+": Not a valid number")
+    if(i==4):
+        while(True):
+            print("Motor to move:Location , type 0 to exit")
+            s = input()
+            if(s=='0'):
+                break
+            s1 = s.split(':')
+            s1[0]=s1[0].replace(':','')
+            s1[1]=s1[1].replace(':','')
+            if(s1[0].find('all')!=-1):
+                for j in range(0,16):
+                    sc.move(j,int(s1[1]))
+            else:
+                sc.move(int(s1[0]),int(s1[1]))
+            
+           
+            
     
     return
 
